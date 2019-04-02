d36d80592cd7016eabf38a478372fae2927f59a2
juziem <lus0800@mail.ru>
2019-04-02 12:15:22 +0700

    title

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace ClassLibrary1
 {
     public class Class1
     {
+
     }
 }
---
+++ ClassLibrary1/site_test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ClassLibrary1
+{
+    [TestFixture]
+    class site_test
+    {
+        IWebDriver webDriver = new ChromeDriver();
+
+        [TestCase]
+        public void mainTitle()
+        {
+            webDriver.Url = "https://www.adobe.com/ru/";
+            Assert.AreEqual("Adobe Россия: решения для творчества, маркетинга и работы с документами", webDriver.Title);
+            webDriver.Close();
+        }
+
+
+
+        [TearDown]
+        public void testEnd()
+        {
+            webDriver.Quit();
+        }
+    }
+}
